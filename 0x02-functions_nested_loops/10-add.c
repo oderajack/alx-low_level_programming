fd202a98818e8d323ed02fab7e5d03e9667136ac
oderajack <matorojack@gmail.com>
2023-02-21 10:52:04 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two
+ * @n1: The first number to be printed
+ * @n2: The second number to be printed
+ * Return: Int
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

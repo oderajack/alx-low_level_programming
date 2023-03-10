89976b5e8130444833e2dbb28adbb820c1bfecc0
oderajack <matorojack@gmail.com>
2023-03-10 09:22:59 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints # of args
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", rgc - 1);
+	
+	return (0);
+}
